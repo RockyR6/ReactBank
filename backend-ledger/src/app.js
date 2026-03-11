4f0b65d8fe8585c408e1b3bdcff2fafc9f2052cf
RockyR6 <rockyroy76r@gmail.com>
2026-03-11 16:53:13 +0530

    client path chenge

--- backend-ledger/src/app.js
+++ backend-ledger/src/app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express()
 
 app.use(cors({
-    origin:[ 'http://localhost:5173', 'https://first-bank-liard.vercel.app/' ],
+    origin:[ 'http://localhost:5173', 'https://first-bank-liard.vercel.app' ],
     credentials: true
 }));
 
